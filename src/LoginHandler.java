08ad19c9ab8b3ec8ae9bfda2de645b91f6fb29ef
DestructeurDeChatte <36827612+DestructeurDeChatte@users.noreply.github.com>
2018-04-08 23:36:54 +1000

    Update LoginHandler.java

--- src/LoginHandler.java
+++ src/LoginHandler.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
 
 public class LoginHandler {
 
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    
+    String Username;
+    String Password;
+    
+    System.out.println("Username: ");
+    Username = input.nextLine();
+    
+    System.out.println("Password: ");
+    Password = input.nextLine();
+
+    
+    if(Username.equals("...") && (Password.equals("...)){
+            System.out.println("logged in");
+    }else{
+      System.out.println("Username/Password incorrect, please try again")
+    }
+    
+  }
+  
+  
+  
 }
