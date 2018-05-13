9a8cbf0e75cde1181580c9d6c95ba850ed241efc
Guy <zandaguy@gmail.com>
2018-05-13 22:14:05 +1000

    Added a class 'Payroll' written by Almog

---
+++ src/Model/Payroll.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Payroll {
+
+    private float hourlyRate;
+    private float shiftHours;
+    private float totalMonthlyPay;
+
+    public Payroll(float hourlyRate, float hours) {
+        this.hourlyRate = hourlyRate;
+        this.shiftHours = hours;
+    }
+
+    public float getRate() {
+        return hourlyRate;
+    }
+
+    public float getHours() {
+        return shiftHours;
+    }
+
+    public float calculatePay(float shiftHours, float hourlyRate) {
+
+        if (shiftHours<= 40){
+            totalMonthlyPay = this.shiftHours * this.hourlyRate;
+        }
+
+
+        return totalMonthlyPay;
+    }
+    public float displaySalary(float totalMonthlyPay)
+    {
+        System.out.print("The Salary is "+ totalMonthlyPay);
+        return totalMonthlyPay;
+    }
+
+
+}
