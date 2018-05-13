a022bcaaf9c1f5d30f19dba2f8c735db9d6f470c
DestructeurDeChatte <36827612+DestructeurDeChatte@users.noreply.github.com>
2018-05-14 00:01:29 +1000

    Update User.java

--- src/Model/User.java
+++ src/Model/User.java
@@ -2,10 +2,41 @@ package Model;
 
 import java.util.Scanner;
 
-public abstract class User {
-	
-	private String name;
-	private String id;
-	private String staffType;
 
+import java.util.*;
+
+public class user {
+
+		private String name;
+		private String staffType;
+		private String username;
+		private String password;
+		private String[][] account = {{"jesper", "abc123","casualStaff"},{"christian", "abc123","approver"}};
+
+		public user(String user, String pass, String staff){
+		    username = user;
+		    password = pass;
+		    staffType = staff;
+		}
+
+		public boolean req(){
+			if((username.equals(account[0][0])) && (password.equals(account[0][1])))
+		        return true;
+		    else
+		        return false;
+		}
+	
+		public void viewMenu() {};
+		public void selectAction() {};
+	
+		public int readMenuInput() {
+			Scanner scanner = new Scanner(System.in);
+		
+			System.out.print("Enter a valid number to select action: ");
+			int input = scanner.nextInt();
+		
+			return input;
+	}
+
+		
 }
